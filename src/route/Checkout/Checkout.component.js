fcb7a1d37f9996260ddee53e118fa6a46c06dddb
Alieltahan <alyaltahan@gmail.com>
2022-01-26 16:25:07 +0200

    git init

---
+++ src/route/Checkout/Checkout.component.js
@@ -0,0 +1,8 @@
+import {Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component';
+import './Checkout.extension.style.scss'
+
+export class Checkout extends SourceCheckout{
+    
+}
+
+export default  Checkout;
